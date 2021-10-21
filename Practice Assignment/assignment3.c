c76fbc5457b4c6fd4ff212d95da87a9febbc4159
Pranav Masekar <83063593+PranavMasekar@users.noreply.github.com>
2021-10-21 14:41:50 +0530

    sets done

--- Practice Assignment/assignment3.c
+++ Practice Assignment/assignment3.c
@@ -1,36 +1,89 @@
-#include<stdio.h>
-#include<strings.h>
-struct product{
-    int code;
-    char name[10];
+#include <stdio.h>
+#include <string.h>
+struct Product
+{
+    char code[10];
+    char name[20];
     int price;
-};
+} product[4];
 
-void getdata(struct product * ptr){
-    printf("Enter the code,price,name of product :");
-    scanf("%d",&ptr->code);
-    scanf(" %d",&ptr->price);
-    scanf("%s",&ptr->name);
+struct BillGenerator
+{
+    char name[10];
+    int quantity;
+    int price;
+} Bill[100];
 
+void setdata()
+{
+    strcpy(product[0].code, "101");
+    strcpy(product[0].name, "Laptop");
+    product[0].price = 35000;
+
+    strcpy(product[1].code, "202");
+    strcpy(product[1].name, "Mobile");
+    product[1].price = 20000;
+
+    strcpy(product[2].code, "303");
+    strcpy(product[2].name, "ipad");
+    product[2].price = 60000;
+
+    strcpy(product[3].code, "404");
+    strcpy(product[3].name, "Alexa");
+    product[3].price = 15000;
 }
-
-void putdata(struct product * ptr){
-    printf("The name of product is %s \n",ptr->name);
-    printf("The code of product is %d \n",ptr->code);
-    printf("The price of product is %d \n",ptr->price);
-}
-int main(){
-    int p;
-    printf("How many products do you want to add to cart :");
-    scanf("%d",&p);
-    struct product data[p];
-    for(int i=0;i<p;i++){
-        getdata(&data[i]);
+int main()
+{
+    char cont = 'y', counter = 0;
+    int finalprice = 0;
+    setdata();
+    printf("\nWelcome to the shop\n\nWhat do you want to buy?\n\n");
+    while (cont == 'y')
+    {
+        printf("Sr.No\tName\tCode\tprice\n");
+        for (int i = 0; i < 4; i++)
+        {
+            printf("%d.\t%s\t%s\tRs.%d\n", i + 1, product[i].name, product[i].code, product[i].price);
+        }
+        int choice, quantity;
+        scanf("%d", &choice);
+        printf("Enter the quantity you want : ");
+        scanf("%d", &quantity);
+        switch (choice)
+        {
+        case 1:
+            strcpy(Bill[counter].name, product[0].name);
+            Bill[counter].quantity = quantity;
+            Bill[counter].price = product[0].price * quantity;
+            break;
+        case 2:
+            strcpy(Bill[counter].name, product[1].name);
+            Bill[counter].quantity = quantity;
+            Bill[counter].price = product[1].price * quantity;
+            break;
+        case 3:
+            strcpy(Bill[counter].name, product[2].name);
+            Bill[counter].quantity = quantity;
+            Bill[counter].price = product[2].price * quantity;
+            break;
+        case 4:
+            strcpy(Bill[counter].name, product[3].name);
+            Bill[counter].quantity = quantity;
+            Bill[counter].price = product[3].price * quantity;
+            break;
+        default:
+            printf("Invalid choice\n");
+        }
+        printf("Do you want to buy more things (y/n) : ");
+        scanf(" %c", &cont);
+        counter++;
     }
-    for(int i=0;i<p;i++){
-        putdata(&data[i]);
+    printf("\nYour Bill is\n");
+    printf("Sr.No.\tName\tQuantity\tPrice\n");
+    for (int i = 0; i < counter; i++)
+    {
+        printf("%d\t%s\t%d\t\t%d\n", i + 1, Bill[i].name, Bill[i].quantity, Bill[i].price);
+        finalprice += Bill[i].price;
     }
-
-
-    return 0;
+    printf("Final Bill : %d", finalprice);
 }
--- Practice Assignment/assignment5.c
+++ Practice Assignment/assignment5.c
@@ -1,7 +1,61 @@
 #include<stdio.h>
+#include<string.h>
+int calclength(char *s){
+    int c=0;
+    while (s[c] != '\0')
+    c++;
+    return c;
+}
+
+void reverse(char *s , int size){
+    char rev[size];
+    int end = size-1;
+    int i;
+    for( i=0;i<size;i++){
+        rev[end] = s[i];
+        end--;
+    }
+    rev[i]='\0';
+    printf("The reversed string is %s\n",rev);
+}
+
+void compare(char *s , char *p){
+    int flag = 1;
+    int s1 = calclength(s);
+    int s2 = calclength(p);
+    if (s1 == s2)
+    {
+        for (int i = 0; i < s1; i++)
+        {
+            if (s[i] != p[i])
+            {
+                flag = 0;
+                break;
+            }
+        }
+    }
+    else
+        flag = 0;
+
+    if(flag==1) printf("Strings are equal \n");
+    else{
+        printf("Strings are not equal \n");
+    }
+}
 
 int main(){
     printf("Enter the string ");
-    
+    char str[20],com[20];
+    scanf("%s",&str);
+
+    int size = calclength(str);
+    printf("The size of the string is %d \n",size);
+
+    reverse(str,size);
+
+    printf("Enter the string to compare ");
+    scanf("%s",&com);
+
+    compare(str,com);
     return 0;
 }
